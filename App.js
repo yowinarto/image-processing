354aa6d09a3c2f62a5d0f46efa30c62967c584de
kukuhbr <kukuhis777@gmail.com>
2018-08-31 04:07:41 +0700

    added victory chart histogram to app

--- App.js
+++ App.js
@@ -1,23 +1,100 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, View } from 'react-native';
+import { VictoryChart, VictoryBar, VictoryTheme } from 'victory-native';
+
+data = [];
+
+var i;
+for(i = 0; i < 255; i++) {
+  data.push({ quarter: i, earnings: Math.floor(Math.random() * 20001) });
+}
+
+data2 = [
+  { quarter: 1, earnings: 13000 },
+  { quarter: 2, earnings: 18720 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 },
+  { quarter: 5, earnings: 13000 },
+  { quarter: 6, earnings: 18720 },
+  { quarter: 7, earnings: 14250 },
+  { quarter: 8, earnings: 19000 }
+]
+data3 = [
+  { quarter: 1, earnings: 13000 },
+  { quarter: 2, earnings: 18720 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 },
+  { quarter: 5, earnings: 13000 },
+  { quarter: 6, earnings: 18720 },
+  { quarter: 7, earnings: 14250 },
+  { quarter: 8, earnings: 19000 },
+  { quarter: 9, earnings: 13000 },
+  { quarter: 10, earnings: 18720 },
+  { quarter: 11, earnings: 14250 },
+  { quarter: 12, earnings: 19000 }
+]
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.wrapper}>
+        <View style={styles.container}>        
+          <VictoryChart width={350} theme={VictoryTheme.material} domainPadding={{x: 25}}>
+            <VictoryBar 
+              style={{
+                data: {
+                  fill: "#FF0000",
+                  stroke: "#960000",
+                  fillOpacity: 0.6,
+                  strokeWidth: 0.1 
+                }
+              }} 
+              data={data} x="quarter" y="earnings" />
+          </VictoryChart>
+          <Text>Image Red Histogram</Text>
+          
+          <VictoryChart width={350} theme={VictoryTheme.material} domainPadding={{x: 25}}>
+          <VictoryBar 
+              style={{
+                data: {
+                  fill: "#00FF00",
+                  stroke: "#009600",
+                  fillOpacity: 0.6,
+                  strokeWidth: 0.1 
+                }
+              }} 
+              data={data2} x="quarter" y="earnings" />
+          </VictoryChart>
+          <Text>Image Green Histogram</Text>
+
+          <VictoryChart width={350} theme={VictoryTheme.material} domainPadding={{x: 25}}>
+            <VictoryBar 
+              style={{
+                data: {
+                  fill: "#0000FF",
+                  stroke: "#000096",
+                  fillOpacity: 0.6,
+                  strokeWidth: 0.1 
+                }
+              }} 
+              data={data3} x="quarter" y="earnings" />
+          </VictoryChart>          
+          <Text>Image Blue Histogram</Text>
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
--- android/app/src/main/java/com/histogram/MainApplication.java
+++ android/app/src/main/java/com/histogram/MainApplication.java
@@ -3,6 +3,9 @@ package com.histogram;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
+import com.horcrux.svg.SvgPackage;
+import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
           new ImagePickerPackage()
       );
     }
